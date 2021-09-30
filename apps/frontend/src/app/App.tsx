9af495e06a3f18180aa1f1b36c29937289564393
Lukas Holzer <lukas.holzer@typeflow.cc>
2021-09-30 17:25:16 +0200

    use pizza lib

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -1,10 +1,12 @@
 import { FunctionComponent } from 'preact';
+import { PIZZA_TOPPINGS } from '@viennajs/menus/pizza';
 import './App.css';
 
 export const App: FunctionComponent = () => {
   return (
     <div className='app'>
       <h1>hello Vienna.js 👋</h1>
+      {/* {JSON.stringify(PIZZA_TOPPINGS, null, 2)} */}
     </div>
   );
 };
---
+++ menus/pizza/src/pizza.ts
@@ -0,0 +1,1 @@
+export const PIZZA_TOPPINGS = ['mozarella', 'tomato sauce', 'basil'];
